1638e6325af91dc6847e12b8e8cd28dee2a69406
kimlucofficial <kimlucofficial@gmail.com>
2026-04-18 11:32:48 +0700

    fix gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -7,27 +7,27 @@ const scenes = [
   {
     title: "THE CITY BREATHES",
     subtitle: "Where stories begin",
-    img: "/images/1.jpg",
+    img: "https://media.discordapp.net/attachments/1488117722658373743/1494671563553902592/3.png?ex=69e41d9e&is=69e2cc1e&hm=0f5f1fa66c5d19cd06b5198cce1e155defb8d6ed7898e5195884d5e1c855f907&=&format=webp&quality=lossless&width=1286&height=1286",
   },
   {
     title: "LAW VS CHAOS",
     subtitle: "Every choice matters",
-    img: "/images/2.jpg",
+    img: "https://media.discordapp.net/attachments/1488117722658373743/1494671563906351206/4.png?ex=69e41d9e&is=69e2cc1e&hm=32fe102663e93a8a443fddd7f2985a2937340378d3e1f20d6bc404055cb82aa9&=&format=webp&quality=lossless&width=1286&height=1286",
   },
   {
     title: "NIGHT LIFE",
     subtitle: "The city never sleeps",
-    img: "/images/3.jpg",
+    img: "https://media.discordapp.net/attachments/1488117722658373743/1494671564266930258/5.png?ex=69e41d9e&is=69e2cc1e&hm=4e92ee0ec76cfa92fa25081ac2dc28969afb74349314176ed313ce228902de5f&=&format=webp&quality=lossless&width=1286&height=1286",
   },
   {
     title: "POWER & CONTROL",
     subtitle: "Who runs the streets?",
-    img: "/images/4.jpg",
+    img: "https://media.discordapp.net/attachments/1488117722658373743/1494671564648743033/6.png?ex=69e41d9e&is=69e2cc1e&hm=d047e40372741e90ea10f8a14ee6ec1816f1a670f08e0496117f2aae8be27c41&=&format=webp&quality=lossless&width=1286&height=1286",
   },
   {
     title: "THE STORY IS YOURS",
     subtitle: "Write your legacy",
-    img: "/images/5.jpg",
+    img: "https://media.discordapp.net/attachments/1488117722658373743/1494671565311316151/8.png?ex=69e41d9e&is=69e2cc1e&hm=b5e81d3124bc721ccb8ba9a33f950f3e99d364a948412a9cb4d5376f89a5ef43&=&format=webp&quality=lossless&width=1286&height=1286",
   },
 ];
 
