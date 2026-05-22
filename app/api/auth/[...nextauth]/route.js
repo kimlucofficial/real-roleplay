bbe9287f6e178c32e598e1ff2417ac8fc3ea7e4c
kimlucofficial <kimlucofficial@gmail.com>
2026-05-22 20:25:45 +0700

    fix discord bot interactions

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
-import NextAuth from 'next-auth';
+import NextAuthModule from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
+const NextAuth = NextAuthModule.default || NextAuthModule;
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
